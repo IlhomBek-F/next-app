a499192be8b4cfceb4c5c5b055e7b1c836ec5680
Ilhomjon <futurewithme1221@gmail.com>
2024-09-09 18:22:33 +0500

    added dashboard page

---
+++ app/components/Increment.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { useState } from "react";
+
+function Increment() {
+    const [count, setCount] = useState(0);
+    let a;
+    if(typeof window !== 'undefined') {
+        a = localStorage.getItem('a')
+    console.log(a)
+
+    }
+   
+    return (
+        <div>
+            <p>{count}</p>
+            <button onClick={() => setCount((prev) => prev + 1)}>Inc</button>
+            <button onClick={() => setCount((prev) => prev - 1)}>Dec</button>
+        </div>
+    )
+}
+
+export {Increment}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link"
+import { Increment } from "./components/Increment"
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Link href='/dashboard'>Dashboard</Link>
+      <Increment />
        <button>Dashboard</button>
     </div>
   );
